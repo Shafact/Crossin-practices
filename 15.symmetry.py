6e86da5c9fc945ac8c4bda13553ec8c9c1690613
Sharon <sharon.xzhou9@gmail.com>
2020-02-26 00:06:51 -0500

    finish 15

---
+++ 15.symmetry.py
@@ -0,0 +1,26 @@
+"""
+问题描述】
+
+回文数：一个数从左向右与从右向左读是一样的，例如34543和1234321都是回文数
+
+问题：输出所有200以内，平方是回文数的数。
+
+例如：26 的平方是 676，为回文数。所以 26 满足我们的条件
+"""
+def reverse_str(strg):
+    rev_str = []
+    for i in strg:
+        rev_str.insert(0,i)
+    return rev_str
+
+rightnumber = []
+for i in range(1, 201):
+    square = str(i*i)
+    square_sep = [char for char in square]
+    square_rev = reverse_str(square)
+    if square_sep == square_rev:
+        rightnumber.append(i)
+        print(f"number {i} and square: {i*i}")
+    else:
+        pass
+print("rightnumber: ",rightnumber)
